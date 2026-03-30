52a1c90afc3fdd398b025635de5ac9504a093db2
Sign0retti <122103643+Sign0retti@users.noreply.github.com>
2026-03-30 17:01:07 -0300

    request mapping commit

--- src/main/java/com/banksimulation/bank_simulation/controller/OpenAccountController.java
+++ src/main/java/com/banksimulation/bank_simulation/controller/OpenAccountController.java
@@ -1,5 +1,11 @@
 package com.banksimulation.bank_simulation.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bank/openaccount")
 public class OpenAccountController {
 
 
